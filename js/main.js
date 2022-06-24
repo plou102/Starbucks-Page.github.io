e91e3c3c57722540500fb717c2744c1fa77a7b99
plou102 <107393773+plou102@users.noreply.github.com>
2022-06-24 14:29:57 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -16,6 +16,7 @@ searchInputEl.addEventListener('blur', function () {
 });
 
 const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top');
 
 window.addEventListener('scroll', _.throttle(function () {
   console.log(window.scrollY);
@@ -26,15 +27,29 @@ window.addEventListener('scroll', _.throttle(function () {
       opacity: 0,
       display: 'none'
     });
+    // 버튼 보이기
+    gsap.to(toTopEl, .2, {
+      x: 0
+    });
   } else {
     // 배지 보이기
     gsap.to(badgeEl, .6, {
       opacity: 1,
       display: 'block'
     });
+    // 버튼 숨기기
+    gsap.to(toTopEl, .2, {
+      x: 100
+    });
   }
 }, 300));
 // _.throttle(함수, 시간)
+
+toTopEl.addEventListener('click', function () {
+  gsap.to(window, .7, {
+    scrollTo: 0
+  });
+});
 
 const fadeEls = document.querySelectorAll('.visual .fade-in');
 fadeEls.forEach(function (fadeEl, index) {
@@ -42,7 +57,7 @@ fadeEls.forEach(function (fadeEl, index) {
     delay: (index + 1) * .7,
     opacity: 1
   });
-});
+}); // 클릭 시 화면 상단으로 이동
 
 
 // new Swiper(선택자, 옵션)
@@ -128,3 +143,6 @@ spyEls.forEach(function (spyEl) {
     .addTo(new ScrollMagic.Controller);
 });
 
+
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear(); // copyright 년도
